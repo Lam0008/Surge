3d92cb1e16439c52eee3cc00ecb3bac916c2cfb4
Lam0008 <97611792+Lam0008@users.noreply.github.com>
2024-01-22 21:53:27 +0800

    Create NYMF_Purchased.js

---
+++ JS/NYMF_Purchased.js
@@ -0,0 +1,66 @@
+[
+  {
+    "id": 55,
+    "email": "mbwmvw55zk@privaterelay.appleid.com",
+    "photo": {
+      "description": "The temperature in the shop was rising by the minute, and small droplets of sweat ran down her velvety skin like a picturesque rivulet, enlivening her athletic physique. Helga was a true professional and skillfully handled the huge lathe as if it were a part of her soul. The sun was already shining high outside and its rays penetrated the workshop, creating bright patches of light.",
+      "model_ids": [
+        193
+      ],
+      "main": "https:\/\/nymfapp.com\/storage\/models\/jpg\/c\/c00bb8602c9e0f21908fbed2fab12d6b.jpg",
+      "class": "photo",
+      "source": "https:\/\/nymfapp.com\/storage\/models\/jpg\/0\/07cedee172df1c72a4edc786d43fe8ad.jpg",
+      "code": "DD-4474174",
+      "country": "",
+      "pro_main": "https:\/\/nymfapp.com\/api\/file\/pro\/121269\/8b3611be72c78f543572f6bd552176cb\/121272\/77ae6056367e60d4c50457c1cd55028f",
+      "series_id": 447,
+      "keywords": "dressed semi-nude undressed nude undresses poses brunette bundle gray eyes nipples nipple breast big naked body standing silhouette beautiful charming attractive sexy one herself young girl turner machine shop workshop suit working T-shirt",
+      "source_data": {
+        "size": 3287927,
+        "ext": "jpg",
+        "ratio": 0.84,
+        "class": "file",
+        "width": 4208,
+        "height": 5008
+      },
+      "specifications": "Premium: JPEG · 1.48 MB · 1613×1920 · Portrait\nOriginal: JPEG · 3.14 MB · 4208×5008 · Portrait",
+      "name": "Turning Shop",
+      "miniature": "https:\/\/nymfapp.com\/storage\/models\/jpg\/8\/8ae4d8ebbe34e3f51921ec7c7b6fa527.jpg",
+      "info": "",
+      "post_ids": [
+        712,
+        711
+      ],
+      "author_id": 1,
+      "id": 4174,
+      "filter_id": "14_Magical Italy",
+      "vr_id": null,
+      "date": 1697976480,
+      "purchase_available": true,
+      "vr": false,
+      "watermark": "https:\/\/nymfapp.com\/storage\/models\/jpg\/0\/07cedee172df1c72a4edc786d43fe8ad.jpg",
+      "censored": true,
+      "width": 1075,
+      "pro_miniature": "https:\/\/nymfapp.com\/storage\/models\/jpg\/c\/c3f3b4ec7f5a27caa12a86bdd6c25a88.jpg",
+      "height": 1280,
+      "ratio": 0.84,
+      "category_id": 29,
+      "sound": null,
+      "pro": true
+    },
+    "license_url": null,
+    "customer": "lam",
+    "transaction_id": "120002112468810",
+    "valid": true,
+    "currency_code": "USD",
+    "source": "https:\/\/nymfapp.com\/api\/file\/purchase\/121271\/286add4c3c081f01adbaae8a95e7dc0f\/121272\/77ae6056367e60d4c50457c1cd55028f",
+    "os": "ios",
+    "price": "14.99",
+    "date": 1705903980,
+    "product_id": "com.nymf.app.photo",
+    "license_type": "personal",
+    "class": "purchase",
+    "device": "73A19112-9540-4D3B-B7E8-1E0EDD7AFB61",
+    "photo_id": 4174
+  }
+]
