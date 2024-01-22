e75ef4645efc43fa3757e249218232be42b3a1a7
Lam0008 <97611792+Lam0008@users.noreply.github.com>
2024-01-22 21:04:50 +0800

    Update NYMF_Photos.js

--- JS/NYMF_Photos.js
+++ JS/NYMF_Photos.js
@@ -1,3 +1,3 @@
 var objc = JSON.parse($response.body);
-objc = objc.replace(/"pro":false/g,""pro":true");
+var objc = objc.replace(/"pro" : false/g,"pro" : true);
 $done({body : JSON.stringify(objc)});
